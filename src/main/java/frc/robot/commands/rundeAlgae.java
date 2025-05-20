b5dfd8150d83cf2fd926da6857ec6c15a40dd4dd
david.hutchsion <oldwateley@gmail.com>
2025-05-19 21:35:14 -0500

    The serial commuincation is working - paired with M4 RSMatrix

--- src/main/java/frc/robot/commands/rundeAlgae.java
+++ src/main/java/frc/robot/commands/rundeAlgae.java
@@ -1,23 +1,40 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.deAlgae;
 
-public class RunDeAlgae extends Command {
-    private deAlgae deAlgae;
-    
-    public RunDeAlgae(deAlgae _deAlgae){
-        addRequirements(_deAlgae);
-        deAlgae = _deAlgae;
+/**
+ * Temporary stub: RunDeAlgae implementation is commented out.
+ */
+public class rundeAlgae extends Command {
+    // --- original implementation commented out below ---
+    /*
+    private final deAlgae m_subsystem;
+
+    public RunDeAlgae(deAlgae subsystem) {
+        m_subsystem = subsystem;
+        addRequirements(m_subsystem);
     }
-    public void initialize(){}
-    public void execute(){
-        deAlgae.runDeAlgae();
+
+    @Override
+    public void initialize() {
+        // …
     }
-    public void end(boolean interrupted){
-        deAlgae.stopDeAlgae();
+
+    @Override
+    public void execute() {
+        m_subsystem.runMotor();
     }
-    public boolean isFinished(){
+
+    @Override
+    public void end(boolean interrupted) {
+        m_subsystem.stopMotor();
+    }
+
+    @Override
+    public boolean isFinished() {
         return false;
     }
+    */
+
+    // Nothing here for now.
 }
