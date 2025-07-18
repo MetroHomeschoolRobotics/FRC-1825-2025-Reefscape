9dc77aea4fef1b0d0a29d8d953e8fb6569528e36
Joseph Bergthold <josephbergthold@gmail.com>
2025-07-17 22:02:43 -0500

    Made a custom PID controller for future tinkering

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -2,14 +2,21 @@ package frc.robot.subsystems;
 // import com.revrobotics.spark.SparkBase;
 
 
+import org.ejml.simple.SimpleMatrix;
+
 // import com.revrobotics.spark.config.SparkBaseConfig;
 // import com.revrobotics.spark.config.SparkMaxConfig;
 
 import com.revrobotics.spark.SparkLowLevel;
 import com.revrobotics.spark.SparkMax;
 
+import edu.wpi.first.math.MatBuilder;
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.Nat;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N4;
+import edu.wpi.first.math.numbers.N5;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -20,12 +27,12 @@ import frc.robot.Constants;
 //190 cm high at apex
 public class Elevator extends SubsystemBase {
     
+    // private PIDController pid = new PIDController(.030, 0.00, 0.001);
+    private Matrix<N5, N1> PIDMatrix = MatBuilder.fill(Nat.N5(), Nat.N1(), .031*12,-0.001*12,0,0,0);
+    private Matrix<N1,N5> errorHistory = MatBuilder.fill(Nat.N1(), Nat.N5(), 0,0,0,0,0);
+    private double elevatorSetPoint = 0;
+    private double elevatorTolerance = 20;
     
-    
-
-    private PIDController pid = new PIDController(.030, 0.00, 0.001);
-
-    //
     private double desiredposition = 0;
     private double highestGetDistance;
 
@@ -48,7 +55,7 @@ public class Elevator extends SubsystemBase {
     public Elevator(){
         
         resetEncoders();
-        pid.setTolerance(25);
+        // pid.setTolerance(25);
         //mayhaps idk, if it doesnt work make the  motor1 speeds negative in periodic()
         //elevatorMotor1.configure(
           //  config, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
@@ -62,18 +69,21 @@ public class Elevator extends SubsystemBase {
         if(speed <=0 && desiredposition>= Constants.elevatorConstants.elevatorMaxHeight && distanceToLimit > Constants.distToLimOffset){
             
             desiredposition += speed;
-            pid.setSetpoint(desiredposition);
+            // pid.setSetpoint(desiredposition);
+            elevatorSetPoint = desiredposition;
             
         } else if(speed>=0 && desiredposition <-93.66  ){//!beambreak.get()
             
              desiredposition += speed;
-             pid.setSetpoint(desiredposition);
+             // pid.setSetpoint(desiredposition);
+             elevatorSetPoint = desiredposition;
             
          }
         
         // SmartDashboard.putNumber("Commanded elevator speed 0-1", speed);
         // SmartDashboard.putNumber("Elevator motor 1 current", elevatorMotor1.getOutputCurrent());
-        pid.setSetpoint(desiredposition);
+        // pid.setSetpoint(desiredposition);
+        elevatorSetPoint = desiredposition;
         
     }
 
@@ -97,21 +107,41 @@ public class Elevator extends SubsystemBase {
     }
     public void setPID(double setPoint){
         desiredposition = setPoint;
-        pid.setSetpoint(desiredposition);
+        // pid.setSetpoint(desiredposition);
+        elevatorSetPoint = desiredposition;
+
         
     }
     public double getSetpoint(){
-        return pid.getSetpoint();
+        // return pid.getSetpoint();
+        return elevatorSetPoint;
     }
     public boolean atSetpoint(){
-        return pid.atSetpoint();
+        // return pid.atSetpoint();
+        if (Math.abs(elevatorSetPoint - getDistance()) < elevatorTolerance) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
     public void periodic(){
         double output;
         double ModifiedOutput;
-        output = pid.calculate(getDistance());
+        // output = pid.calculate(getDistance());
+        Matrix<N1, N5> helperMatrix = new Matrix<>(new SimpleMatrix(1, 5));
+        helperMatrix.set(0, 0, elevatorSetPoint - getDistance());
+        Matrix<N1, N4> slice = errorHistory.block(0, 0, 1, 4);
+        helperMatrix.getStorage().insertIntoThis(0, 1, slice.getStorage());
+        errorHistory = helperMatrix;
+
+        output = errorHistory.times(PIDMatrix).get(0, 0); //this is in volts
+        output = output/12;
+
+
+
         // Custom feedforward that modifies based on the angle of the shoulder
-        if(pid.getSetpoint()<-98.66){
+        if(elevatorSetPoint<-98.66){
             ModifiedOutput = output-0.17*Math.cos(Math.toRadians(ShoulderPID.getAbsoluteAngle()));
         }else{
             ModifiedOutput = output;
@@ -120,7 +150,7 @@ public class Elevator extends SubsystemBase {
         elevatorMotor1.set(ModifiedOutput);
         elevatorMotor2.set(-ModifiedOutput);
 
-        SmartDashboard.putNumber("elevator Desired Pos", pid.getSetpoint());
+        SmartDashboard.putNumber("elevator Desired Pos", elevatorSetPoint);
         // SmartDashboard.putNumber("elevator error", pid.getError());
         SmartDashboard.putNumber("elevator Distance", getDistance());
          SmartDashboard.putNumber("elevator Encoder Value", getEncoder());
